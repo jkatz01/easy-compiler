a9a229101ef7698771c71db70e9cb648af174895
Yehonatan Katz <yehonatankatz@gmail.com>
2024-01-25 23:23:18 -0500

    Documnetation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "Lexer.cpp"
-
+int coolFunction(int n);
+/// <summary>
+/// Main
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
+/// <returns></returns>
 int main(int argc, char* argv[]) {
 	// Assert number of arguments
 	if (argc != 2) {
@@ -12,5 +18,16 @@ int main(int argc, char* argv[]) {
 	if (lex_err_count > 0) {
 		std::cout << "Lexical analysis failed with " << lex_err_count << " errors" << std::endl;
 	}
+	coolFunction(5);
 	return 0;
-}	
+}
+
+/// <summary>
+/// This doesn't do anything i just dont know how to use doxygen
+/// </summary>
+/// <param name="n"></param>
+/// <returns></returns>
+int coolFunction(int n) {
+	n = n + n;
+	return n;
+}
